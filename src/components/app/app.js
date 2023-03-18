922457fc8819c66f7767c7883afc9d654f01c936
pavlonedaskivskij8@gmail.com <90754825+pavelneda@users.noreply.github.com>
2023-03-19 00:01:58 +0200

    change depedency

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Routes, Route, Link } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import './app.css';
 
 
@@ -8,13 +8,7 @@ import Footer from "../footer/footer";
 import About from "../about/about";
 import Courses from "../courses/courses";
 import CoursePage from "../course-page/course-page";
-
-import PersonDetails from "../person-details/person-details";
-import ItemList from "../item-list/item-list";
-import RandomPlanet from "../random-planet/random-planet";
-import ErrorButton from "../error-button/error-button";
 import ErrorIndicator from "../error-indicator/error-indicator";
-import PeoplePage from "../people-page/people-page";
 
 
 export default class App extends Component {
